228c6495e546da950dce3cfd6dfd77b37edfdf3c
gabe fantacone <gabrielfantacone@students.berkeley.net>
2018-11-14 14:27:13 -0800

    REVIEW BEFORE SUBMITTING

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ function calculateGradeNeeded(){
     var currentWeighted = parseInt(document.getElementById("currentOutput").innerHTML) * nonFinalWeight;
     var gradeWanted = parseInt(document.getElementById("gradeWanted").value);
     var finalNeeded = (gradeWanted - currentWeighted) / finalPercent;
-    document.getElementById("finalOutput").innerHTML = "You need a " + finalNeeded + " on the final to get a " + gradeWanted + " in the class.";
+    if (finalNeeded < 0) {
+        document.getElementById("finalOutput").innerHTML = "You could get a zero on the final and still have at least a " + gradeWanted + " in the class. Don't sweat it!";
+    } else if (isNaN(finalNeeded) || isNaN(gradeWanted)){
+        alert("The program has failed gracefully. Please enter NUMERIC values.");
+    } else {
+        document.getElementById("finalOutput").innerHTML = "You need a " + Math.floor(finalNeeded) + " on the final to get a " + gradeWanted + " in the class.";
+    }
 }
 
 function calculateCurrentGrade() {
@@ -17,38 +23,38 @@ function calculateCurrentGrade() {
         gradeValues[i] = averageArray(gradeNumbers);
         if (gradeValues[i] > 120){
             alert("One of your grade values is too high. Please change it to be lower than 120.");
-            return false;
+            return null;
         } else {
-            var color = document.getElementById(i.toString()).style.backgroundColor;
+            var currentRow = document.getElementById(i.toString());
             if (gradeValues[i] >= 90 && gradeValues[i] <= 110){
-                color = "#51b91d";
-            } else if (gradeValues[i] > 90 && gradeValues[i] <= 80) {
-                color = "#2dc2dc";
-            } else if (gradeValues[i] > 80 && gradeValues[i] <= 70){
-                color = "#e5e066";
+                currentRow.style.backgroundColor = "#51b91d";
+            } else if (gradeValues[i] < 90 && gradeValues[i] >= 80) {
+                currentRow.style.backgroundColor = "#2dc2dc";
+            } else if (gradeValues[i] < 80 && gradeValues[i] >= 70){
+                currentRow.style.backgroundColor = "#e5e066";
             } else {
-                color = "#ff0000";
+                currentRow.style.backgroundColor = "#ff0000";
             }
         }
     }
     var weights = document.getElementsByClassName("percents");
     var weightValues = [];
     for (var j = 0; j < weights.length; j++){
-        weightValues[j] = (weights[j].value / 100);
         if (weightValues[j] > 1){
             alert("One of your percents is greater than 100. Please change it.");
-            return false;
+            return null;
         } else {
-            var sum = 0;
-            console.log(weightValues);
-            for (var k = 0; k < weightValues.length; k++){
-                sum += weightValues[k];
-            }
-            if (sum != 1){
-                alert("Your percents do not add up to 100. Please change them.");
-                return false;
-            }
+            weightValues[j] = (weights[j].value / 100);
         }
+    }
+    var sum = 0;
+    console.log(weightValues);
+    for (var k = 0; k < weightValues.length; k++){
+        sum += weightValues[k];
+    }
+    if (sum != 1){
+        alert("Your percents do not add up to 100. Please change them.");
+        return null;
     }
     var total = 0;
     for (var l = 0; l < gradeValues.length; l++) {
@@ -81,7 +87,8 @@ function reset(){
     table.appendChild(initRow);
     initRow.appendChild(initCell1);
     initRow.appendChild(initCell2);
-    initRow.setAttribute("id", ROWCOUNT.toString());
+    initRow.style.backgroundColor = "black";
+    initRow.style.color = "white";
     document.getElementById("finalWeight").style.visibility = "hidden";
     document.getElementById("calcNeeded").style.visibility = "hidden";
     document.getElementById("gradeWanted").style.visibility = "hidden";
@@ -99,9 +106,12 @@ function addRow(){
         var titleCell2 = document.createElement("td");
         titleCell1.innerHTML = title + " Points";
         titleCell2.innerHTML = title + " Weight";
+        titleCell1.style.color = "white";
+        titleCell2.style.color = "white";
         table.appendChild(titleRow);
         titleRow.appendChild(titleCell1);
         titleRow.appendChild(titleCell2);
+        titleRow.setAttribute("id", ROWCOUNT.toString());
         document.getElementById("categoryName").value = "";
 
         //input row
@@ -153,9 +163,9 @@ function calcEvenWeight(rowCount){
     }
 }
 
-function disable(className){
+function disable(selector){
     document.getElementById("addRow").isDisabled = true;
-    document.getElementById("addRow").className = className;
+    document.getElementById("addRow").className = selector;
     document.getElementById("calcGrade").isDisabled = true;
-    document.getElementById("calcGrade").className = className;
+    document.getElementById("calcGrade").className = selector;
 }
